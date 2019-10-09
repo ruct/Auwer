cf026a3db46dfa40cd5bc5df7c45f10e66e4ed5b
ltry <flyce32@yandex.ru>
2019-10-09 21:08:27 +0300

    population & kernel classes implementation started

--- v3/main.cpp
+++ v3/main.cpp
@@ -2,16 +2,20 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <algorithm>
 
-using namespace std;
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
 
-std::mt19937 GEN(3711);
+std::mt19937 GGEN(3711);
 const int WH = 512;
 const int SIZE = 64;
 const string NAME = "data_test1_blank";
 const int NL = 2400, NR = 2699;
 const int CNTSQ = WH/SIZE;
-
+typedef double ld;
 
 namespace augm {
     int read_int(std::ifstream&);
@@ -32,6 +36,7 @@ struct square {
     }
 };
 
+//  reading picture
 square PICTURE[CNTSQ][CNTSQ];
 namespace augm {
     int read_int(std::ifstream& in) {
@@ -61,9 +66,7 @@ namespace augm {
     }
 }
 
-
-
-typedef double ld;
+//  computations
 namespace ga {
     int dx[] = {0, 0, 1, -1},
         dy[] = {1, -1, 0, 0};
@@ -83,21 +86,49 @@ namespace ga {
 #define sqr(a) ((a)*(a))
 #define cbr(a) ((a)*(a)*(a))
 
-    inline ld metric(std::vector <int> const& a,
-                    std::vector <int> const& b) {
+    inline ld metric(int a[3][SIZE], int b[3][SIZE]) {
         ld result = 0;
-        for (int T = 0; T < a.size(); ++T)
-            result += ld(sqr(a[T]-b[T]));
+        for (int c = 0; c < 3; ++c)
+        for (int T = 0; T < SIZE; ++T)
+            result += ld(sqr(a[c][T]-b[c][T]));
         return sqrt(result);
     }
+    void compute() {
+        for (int p1 = 0; p1 < CNTSQ*CNTSQ; ++p1)
+        for (int p2 = 0; p2 < CNTSQ*CNTSQ; ++p2) {
+            if (p1 == p2) continue;
 
+            int i1 = p1/CNTSQ, j1 = p1%CNTSQ;
+            int i2 = p2/CNTSQ, j2 = p2%CNTSQ;
+
+            ld cur_diss;
+            int a[3][SIZE], b[3][SIZE];
+
+            for (int c = 0; c < 3; ++c)
+            for (int i = 0; i < SIZE; ++i) {
+                a[c][i] = PICTURE[i1][j1].p[c][i][SIZE-1];
+                b[c][i] = PICTURE[i2][j2].p[c][i][0];
+            }
+            cur_diss = metric(a, b);
+            diss[0][p1][p2] = diss[1][p2][p1] = cur_diss;
+
+            for (int c = 0; c < 3; ++c)
+            for (int j = 0; j < SIZE; ++j) {
+                a[c][j] = PICTURE[i1][j1].p[c][SIZE-1][j];
+                b[c][j] = PICTURE[i2][j2].p[c][0][j];
+            }
+            cur_diss = metric(a, b);
+            diss[2][p1][p2] = diss[3][p2][p1] = cur_diss;
+        }
+    }
 }
 
+//  chromosome
 namespace ga {
     struct chromo {
         int perm[CNTSQ][CNTSQ];
         int wher[CNTSQ*CNTSQ];
-        ld eval = -1;
+        mutable ld eval = -1;
 
         bool operator!=(chromo const& o) const {
             for (int i = 0; i < CNTSQ; ++i)
@@ -115,14 +146,109 @@ namespace ga {
             for (int i = 0; i < CNTSQ; ++i) {
                 for (int j = 0; j < CNTSQ; ++j)
                     out << perm[i][j] << "\t";
-                out << std::endl;
+                out << endl;
             }
         }
 
+        ld fit() const {
+            if (eval != -1)
+                return eval;
+
+            eval = 0;
+            for (int i = 0; i < CNTSQ; ++i)
+            for (int j = 0; j < CNTSQ; ++j) {
+                if (j+1 < CNTSQ)
+                    eval += diss[0][perm[i][j]][perm[i][j+1]];
+                if (i+1 < CNTSQ)
+                    eval += diss[2][perm[i][j]][perm[i+1][j]];
+            }
+        }
     };
+
+    void random_chromo(chromo& trg) {
+        vector <int> cont(CNTSQ*CNTSQ);
+        std::iota(cont.begin(), cont.end(), 0);
+        std::shuffle(cont.begin(), cont.end(), GGEN);
+
+        for (int i = 0; i < CNTSQ; ++i)
+        for (int j = 0; j < CNTSQ; ++j)
+            trg.perm[i][j] = cont[i*CNTSQ+j];
+        trg.make_wher();
+    }
+}
+
+
+const int CORES = 4;
+const int GENS = 100;
+const int POP = 1000;
+const int PREAP = 40;
+
+//  population & kernel
+namespace ga {
+    struct population {
+        int nogen;
+        vector <chromo> stock;
+
+        void init() {
+            nogen = 0;
+            stock.resize(POP);
+
+            for (int i = 0; i < POP; ++i)
+                random_chromo(stock[i]);
+        }
+    };
+    struct kernel {
+        static const int TCNTSQ = 2*CNTSQ+1;
+
+    private:
+        int located = 0;
+        vector <int> edge, plan;
+        int perm[TCNTSQ][TCNTSQ];
+        int mni = TCNTSQ, mxi = -1;
+        int mnj = TCNTSQ, mxj = -1;
+
+    public:
+        void write();
+        kernel();
+
+        //  assuming i in [0, TCNTSQ), j in [0, TCNTSQ)
+        int get(int i, int j) const { return perm[i][j]; }
+        bool can(int i, int j) const {
+            if (perm[i][j] != -1)
+                return false;
+
+            if (mxi-mni+1 == CNTSQ && (i < mni || i > mxi))
+                return false;
+            if (mxj-mnj+1 == CNTSQ && (j < mnj || j > mxj))
+                return false;
+            return true;
+        }
+        void place(int i, int j) {
+
+        }
+    };
+    kernel::kernel() {
+        for (int i = 0; i < TCNTSQ; ++i)
+        for (int j = 0; j < TCNTSQ; ++j)
+            perm[i][j] = -1;
+    }
+    void kernel::write() {
+        for (int i = 0; i < TCNTSQ; ++i) {
+            for (int j = 0; j < TCNTSQ; ++j)
+                cout << perm[i][j] << "\t";
+            cout << endl;
+        }
+    }
 
 }
 
+//  genetic operators
+namespace ga {
+    void mutate(chromo& t, std::mt19937& gen);
+    void cross(chromo const& a, chromo const& b, std::mt19937& gen);
+
+
+}
 int main(){
 
 }
